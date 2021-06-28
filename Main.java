751ae48cc438dfaa71da1517f40c2cbdd8a72f07
Noah Christopher <53131326+NSChristopher@users.noreply.github.com>
2021-06-28 19:52:03 -0400

    finished

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        int n = args.length == 1 ? Integer.parseInt(args[0]) : 1;
+        int n = args.length == 1 ? Integer.parseInt(args[0]) : 10;
 
         for (int i = 1; i <= n; ++i) {
             int[] arr = Pascal.triangle(i);
--- Pascal.java
+++ Pascal.java
@@ -1,8 +1,20 @@
 public class Pascal {
     public static int[] triangle(int n) {
 
-        return new int[]{0};
+        var array = new int[n];
+
+        array[0] = 1;
+        if (n > 1) {
+            array[n - 1] = 1;
+            if (n > 2) {
+                for (int i = 1; i < n - 1; ++i) {
+                    array[i] = triangle(n-1)[i - 1] + triangle(n - 1)[i];
+                }
+                return array;
+            }
+            return array;
+        }
+        return array;
+        
     }
 }
-
-// https://javawithus.com/programs/pascal-triangle
